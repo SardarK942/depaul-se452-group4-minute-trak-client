550a28424f5d3b7e204ba306fc63b0c14691c37b
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-05-01 22:14:53 -0500

    Add total column on timesheet

--- src/components/timesheetPage/timesheetForm/TimesheetForm.tsx
+++ src/components/timesheetPage/timesheetForm/TimesheetForm.tsx
@@ -22,9 +22,11 @@ interface TimesheetFormProps {
 function TimesheetForm({ data, tId, projects }: TimesheetFormProps) {
   const [timesheetDetail, setTimesheetDetail] = useState<TimesheetDetail>(data);
   const [isModalOn, setModal] = useState<boolean>(false);
+  const [columns, setColumns] = useState<Column[]>([]);
 
   useEffect(() => {
     setTimesheetDetail(data);
+    setColumns(generateColumns(data));
   }, [data]);
 
   function handleChangeHourSelect(index: number, prop: string, newValue: number): void {
@@ -71,7 +73,16 @@ function TimesheetForm({ data, tId, projects }: TimesheetFormProps) {
       prop: formatISO(curr, { representation: 'date' }),
       label: format(curr, 'MM/dd'),
     });
+    return result;
+  }
 
+  function calculateRowTotal(aRow: { [prop: string]: number }, columns: Column[]): number {
+    let result: number = 0;
+    columns.forEach((column) => {
+      if (aRow[column.prop]) {
+        result += aRow[column.prop];
+      }
+    });
     return result;
   }
 
@@ -100,28 +111,32 @@ function TimesheetForm({ data, tId, projects }: TimesheetFormProps) {
               <th style={{ width: '20rem' }} key="projectName">
                 Project
               </th>
-              {generateColumns(timesheetDetail).map((column) => (
-                <th key={column.prop}>{column.label}</th>
+              {columns.map((column) => (
+                <th key={column.prop} style={{ fontSize: '0.875rem' }}>
+                  {column.label}
+                </th>
               ))}
-              {!timesheetDetail.isSubmitted && <th>Remove</th>}
+              <th style={{ width: '4rem' }}>Total</th>
+              {!timesheetDetail.isSubmitted && <th style={{ width: '4rem' }}>DELETE</th>}
             </tr>
           </thead>
 
           <tbody>
-            {/* RENDER PTO */}
+            {/* RENDER PTO ROW */}
             <tr className={styles.pto_tr}>
               <td>PTO</td>
-              {generateColumns(timesheetDetail).map((column) => (
+              {columns.map((column) => (
                 <td id={column.prop}>{timesheetDetail.pto.hours[column.prop]}</td>
               ))}
-              {!timesheetDetail.isSubmitted && <td></td>}
+              <td>{calculateRowTotal(data.pto.hours, columns)}</td>
+              {!timesheetDetail.isSubmitted && <td />}
             </tr>
 
-            {/* RENDER WORK HOURS */}
+            {/* RENDER WORKS ROW*/}
             {timesheetDetail.works.map((work, idx) => (
               <tr key={work.pId}>
                 <td>{getProjectName(work.pId)}</td>
-                {generateColumns(timesheetDetail).map((column) => (
+                {columns.map((column) => (
                   <TdSelectHour
                     idx={idx}
                     prop={column.prop}
@@ -130,6 +145,7 @@ function TimesheetForm({ data, tId, projects }: TimesheetFormProps) {
                     isReadOnly={timesheetDetail.isSubmitted}
                   />
                 ))}
+                <td>{calculateRowTotal(work.hours, columns)}</td>
                 {!timesheetDetail.isSubmitted && (
                   <td>
                     <Button size="small" color="warning" onClick={() => handleDeleteRow(work.pId)}>
