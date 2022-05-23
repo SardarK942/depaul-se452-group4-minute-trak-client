5160b7c6d64f2740d4881b899ec50a75c564ed8d
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-05-23 14:50:24 -0500

    Add logout

--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -1,13 +1,21 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 import styles from './Navbar.module.css';
 
 function Navbar() {
+  const navigate = useNavigate();
+
   function navLinkStyle(styles: { isActive: boolean }) {
     return {
       backgroundColor: styles.isActive ? '#ffffff80' : '',
       padding: '0.5rem 1rem',
       borderRadius: '12px',
     };
+  }
+
+  function handleClickLogout() {
+    alert('Logout');
+    sessionStorage.clear();
+    navigate('/');
   }
 
   return (
@@ -25,7 +33,7 @@ function Navbar() {
         <NavLink className={styles.item} to="./profile" style={navLinkStyle}>
           Profile
         </NavLink>
-        <div className={`${styles.item} ${styles.logout}`} onClick={() => alert('logout')}>
+        <div className={`${styles.item} ${styles.logout}`} onClick={handleClickLogout}>
           Logout
         </div>
       </div>
