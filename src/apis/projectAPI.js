a096b57a13a2b1c71d1fee3ad15effbd4ca28abb
Wonjun Kang <michael.wonjun.kang@gmail.com>
2022-05-23 14:49:54 -0500

    Add axios utility object

---
+++ src/apis/projectAPI.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import { getSessionToken } from '../utility/common';
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+export default axios.create({
+  baseURL: `${BASE_URL}/project`,
+  headers: {
+    Authorization: `Bearer ${getSessionToken()}`,
+  },
+  withCredentials: true,
+});
